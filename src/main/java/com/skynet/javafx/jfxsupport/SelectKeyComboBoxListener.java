4cb3cfacc7f2b378e65d81f8e6909b2a3c9e135f
tioluwa <tolucollins@gmail.com>
2022-06-24 09:24:23 +0100

    Recompiled for Java 8

--- src/main/java/com/skynet/javafx/jfxsupport/SelectKeyComboBoxListener.java
+++ src/main/java/com/skynet/javafx/jfxsupport/SelectKeyComboBoxListener.java
@@ -1,10 +1,11 @@
 package com.skynet.javafx.jfxsupport;
 
+import com.sun.javafx.scene.control.skin.ComboBoxListViewSkin;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
-import javafx.scene.control.skin.ComboBoxListViewSkin;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
